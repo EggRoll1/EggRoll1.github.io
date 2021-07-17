c317bff9f809807965c7a01fe6deb91f99546ccd
EggRoll1 <46606189+EggRoll1@users.noreply.github.com>
2021-07-16 21:36:07 -0400

    Create randomImg.js

---
+++ randomImg.js
@@ -0,0 +1,14 @@
+
+  var pics =["/img/astronaut-e1553888957531.jpg"];
+
+  var img = '';
+  for(ii=0; ii<pics.length; ii++) {
+    positiontop = Math.random()*500;
+    positionright = Math.random()*1200;
+    zindex = ii;
+
+    img += '<img src="'+pics[ii]+'" style="top: '+positiontop+'px;'+'right: '+positionright+'px;'+'z-index: '+zindex+';">';
+  }
+
+  
+document.getElementById('area').innerHTML= img;
